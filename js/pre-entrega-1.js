9f675ab18ffe5ee9391fa0b34b96f8f49e25492f
Sofia Ambort <sofiambort.programacion@gmail.com>
2023-07-23 22:13:32 -0300

    hice cambios en mi proyecto para la segunda pre-entrega

--- js/pre-entrega-1.js
+++ js/pre-entrega-1.js
@@ -2,6 +2,7 @@ let diaDeLaSemana = prompt ("Ingrese un dia de la semana del Lunes al Viernes");
 let obrasocial = prompt ("Tiene obra social? ingrese si o no");
 let edad = prompt ("Ingrese su edad");
 
+
 switch (diaDeLaSemana) {
         case "Lunes":
             console.log("Hay un turno disponible a las:");
@@ -82,7 +83,10 @@ if (edad >= 18) {
     console.log("Por ser menor de edad, debes presentarte con un adulto")
 }
 
+
+
 let especialidad = prompt ("Ingresa la especialidad Clinico/Pediatria/Dermatología/Ginecología");
+
 
 switch (especialidad){
     case "Clínico":
@@ -110,3 +114,48 @@ switch (especialidad){
     medicoEspecialista ("Dra. Belén ", "Eberhardt");
         break;
 }
+
+alert ("Ingresa tus datos personales para reservar tu turno");
+
+function datosDelPaciente () {
+    const datos = [];
+
+    let nombrepaciente = prompt ("Ingrese su nombre");
+    let apellidopaciente = prompt ("Ingrese su apellido");
+    let direccionDelPaciente = prompt ("Ingrese su dirección");
+    let correoElectronico = prompt ("Ingresa tu email");
+    let edadpaciente = parseInt(prompt ("Ingresa tu edad"));
+    let tipoDeObraSocial = prompt ("Ingresa el nombre de tu obra social");
+    let turnoAreservar = prompt ("Elige uno de los turnos disponibles");
+
+    datos.push(nombrepaciente, apellidopaciente, direccionDelPaciente, correoElectronico, edadpaciente, tipoDeObraSocial, turnoAreservar);
+
+    return datos;
+}
+
+let paciente = datosDelPaciente ();
+console.log("Los datos que ingresaste para la reserva de tu turno son: ");
+console.log(paciente);
+
+alert ("Tu turno ha sido reservado con éxito!");
+
+let zonaIngresada = prompt("Ingresa una zona: CABA / Fuera de CABA para ver que centro mas cercano tienes");
+
+const centroSegunTuZona = [
+    {zona1: "CABA", lugares1: ["Las Heras 2440 - Recoleta", "Juan Maria Gutierrez 3133 - Palermo", "Av. Rivadavia 3802 - Almagro", "Tomas Le Breton 4476 - Villa Urquiza"]},
+    {zona2: "Fuera de CABA", lugares2: ["Tucumán 1140 - Lanús", "Liniers 1821 - San Martín", "9 de Julio 850 - Morón", "Gral. M. Miguel de Güemes 929 - Vicente López"]},
+]
+
+switch (zonaIngresada) {
+    case "CABA": 
+        centroSegunTuZona.forEach((centroSegunTuZona) => {
+            console.log(centroSegunTuZona.lugares1);
+        });
+        break;
+    case "Fuera de CABA":
+        centroSegunTuZona.forEach((centroSegunTuZona) => {
+            console.log(centroSegunTuZona.lugares2);
+        });
+        break;
+}
+
